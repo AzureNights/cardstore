b3ad193d9efd8c68c8e6365797e1a6042b5cc533
azurenights <kokilavanielamparidhi@gmail.com>
2023-03-26 15:55:30 +0800

    Added components and materials and bootstrap

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,14 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { ProductinfoComponent } from './productinfo/productinfo.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'productinfo/productId', component: ProductinfoComponent},
+  {path: 'checkout', component: CheckoutComponent}
+]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    CheckoutComponent,
+    ProductinfoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    RouterModule. forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
