bac55a26617affa1a9a7916858a53030357d4d2e
azurenights <kokilavanielamparidhi@gmail.com>
2023-04-24 18:15:55 +0800

    Added front end to link product info

--- client/src/app/productinfo/productinfo.component.ts
+++ client/src/app/productinfo/productinfo.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CardDetails } from '../models';
+import { Subscription } from 'rxjs';
+import { CardDeetsService } from '../card-deets.service';
 
 @Component({
   selector: 'app-productinfo',
@@ -7,9 +11,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductinfoComponent implements OnInit {
 
-  //product!: Product;
+  params$!: Subscription
+  card: CardDetails[] = []
+  //activatedRoute: any;
+
+  constructor(private cardSvc: CardDeetsService) { }
 
   ngOnInit(): void {
-      
+
+    this.params$ = this.activatedRoute.params.subscribe(
+      (params) =>{
+        const product_id = params['product_id']
+        this.cardSvc.getCardById(product_id)
+          .then(result => {
+            this.card = result
+            console.info('>>> card: ', this.card)
+          })
+          .catch(error => {
+            console.error('>> error: ', error)
+          })
+    }
+
   }
+
 }
+
+
+
